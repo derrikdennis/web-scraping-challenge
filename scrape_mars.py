374469ca130eb5d3f0fcf1d344f3be16f0de878b
Derrik Dennis <derrik@austin.rr.com>
2020-10-25 18:36:29 -0500

    adding mars_news function

--- scrape_mars.py
+++ scrape_mars.py
@@ -25,3 +25,24 @@ def scrape_all():
     # Stop webdriver and return data
     browser.quit()
     return data
+
+
+def mars_news(browser):
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        slide_elem = news_soup.select_one("ul.item_list li.slide")
+        news_title = slide_elem.find("div", class_="content_title").get_text()
+        news_p = slide_elem.find(
+            "div", class_="article_teaser_body").get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
