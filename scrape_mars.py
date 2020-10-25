23ba8b8b283df9141b9da4eac88f9aa04c956c56
Derrik Dennis <derrik@austin.rr.com>
2020-10-25 18:30:01 -0500

    Adding scrape_all def

--- scrape_mars.py
+++ scrape_mars.py
@@ -4,3 +4,24 @@ import pandas as pd
 import datetime as dt
 import time
 import re
+
+
+def scrape_all():
+
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromdriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "hemispheres": hemisphers(browser),
+        "weather": twitter_weather(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
